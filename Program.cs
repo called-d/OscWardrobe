5c7aadd04f186d934cac0d19f7dd9197a06854e2
called_D <called.0xd@gmail.com>
2025-03-29 22:45:52 +0900

    mod: とりあえず oscQuery.Build()

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Timers;
+using VRC.OSCQuery;
+using Microsoft.Extensions.Logging;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var oscQuery = new OSCQueryServiceBuilder()
+    .WithDefaults()
+    .WithTcpPort(Extensions.GetAvailableTcpPort())
+    .WithUdpPort(Extensions.GetAvailableUdpPort())
+    .WithServiceName("OscWardrobe")
+    .Build();
+var refreshTimer = new System.Timers.Timer(5000);
+refreshTimer.Elapsed += (s,e) =>
+{
+	oscQuery.RefreshServices();
+};
+
+Console.WriteLine($"OSCQuery service running on port {oscQuery.TcpPort}");
+
+// Console.ReadKey();
+oscQuery.Dispose();
+
+refreshTimer.Start();
+System.Threading.Thread.Sleep(10_000);
